6936143d27973371e87cd2cb132382bbbb9a475a
Markus Klein <markus-klein@live.de>
2024-04-18 23:50:28 +0200

    add StatusMonitor

--- tests/progress_bar.rs
+++ tests/progress_bar.rs
@@ -9,38 +9,57 @@
 use std::future::ready;
 
 use futures::{Future, FutureExt};
-use tokio::time::{sleep, timeout, Duration};
+use tokio::time::{timeout, Duration};
 
 /// A port implemented by an adapter observing the status of long running
 /// operation.
-pub trait ProgressMonitor {
-    fn status() -> Status;
+pub trait StatusMonitor {
+    fn status(&mut self) -> impl Future<Output = Status>;
 }
 
+#[derive(Clone,Copy)]
 struct Status {
-    expected_total_work: u64,
-    already_done: u64,
+    _progress: u64
 }
 
-pub struct DisplayStatusApp {}
+pub struct DisplayStatusApp<M> {
+    status_monitor: M,
+}
 
-impl DisplayStatusApp {
-    pub fn new() -> Self {
-        DisplayStatusApp {}
+impl<M> DisplayStatusApp<M> {
+    pub fn new(status_monitor: M) -> Self {
+        DisplayStatusApp { status_monitor }
     }
 
-    pub async fn run(&mut self, until: impl Future) {
+    pub async fn run(&mut self, until: impl Future) where M: StatusMonitor{
         tokio::pin!(until);
         while until.as_mut().now_or_never().is_none() {
-            sleep(Duration::from_millis(5)).await
+            let _ = timeout(Duration::from_millis(5), self.status_monitor.status()).await;
         }
+    }
+}
+
+struct ProgressStub {
+    status: Status,
+}
+
+impl ProgressStub {
+    pub fn new(status: Status) -> Self {
+        ProgressStub {status}
+    }
+}
+
+impl StatusMonitor for ProgressStub {
+    async fn status(&mut self) -> Status {
+        self.status
     }
 }
 
 #[tokio::test]
 async fn terminate_the_application() {
     // Given an application.
-    let mut app = DisplayStatusApp::new();
+    let status = ProgressStub::new(Status {_progress: 42 });
+    let mut app = DisplayStatusApp::new(status);
 
     // When we start the application and terminate it immediately
     let run_to_completion = app.run(ready(()));
@@ -50,3 +69,9 @@ async fn terminate_the_application() {
     assert!(result.is_ok())
 }
 
+#[tokio::test]
+async fn display_initial_status() {
+    // Given
+    let status = ProgressStub::new(Status {_progress: 42 });
+    let _app = DisplayStatusApp::new(status);
+}
