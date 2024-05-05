c31c51e56c481100259fff9e113b3d066b9932aa
Markus Klein <markus-klein@live.de>
2024-05-05 16:00:47 +0200

    Create DummyEvent type

--- tests/integration.rs
+++ tests/integration.rs
@@ -2,4 +2,15 @@ use thisactor::Service;
 
 /// A piece of dummy Domain logic to test deriving.
 #[derive(Service)]
-struct Hello;
+pub struct Hello;
+
+impl Hello {
+    pub fn greet(&self) -> &'static str {
+        "Hello, World!"
+    }
+}
+
+#[test]
+fn hello_event_should_exist() {
+    let event = HelloEvent::MyEvent;
+}
--- thisactor-derive/src/lib.rs
+++ thisactor-derive/src/lib.rs
@@ -1,6 +1,21 @@
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Service)]
 pub fn derive_service(item: TokenStream) -> TokenStream {
-    "".parse().unwrap()
+    let input = parse_macro_input!(item as DeriveInput);
+
+    let struct_name = input.ident;
+    let struct_name_str = struct_name.to_string();
+    let event_enum_name = Ident::new(&format!("{struct_name_str}Event"), Span::call_site());
+
+    let expanded = quote!(
+        pub enum #event_enum_name {
+            MyEvent
+        }
+    );
+
+    expanded.into()
 }
